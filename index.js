a2cb11864c13ac2186323a8316a1b107e10a429c
avnishkaushik111 <avnishkaushik111@gmail.com>
2020-02-13 23:55:44 +0530

    latest code

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const shell = require('shelljs');
 const fs = require('fs');
-const path = require('path');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+// const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended : false }));
+
 app.get('/', (req, res) => {
-    fs.chmod('./gitauto.sh', 0o666, err => {
-        if(err) throw err;
-        console.log('File permission changed');
-        shell.exec('sh ./gitauto.sh');
-    });
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    // fs.chmod('./gitauto.sh', 0o666, err => {
+    //     if(err) throw err;
+    //     console.log('File permission changed');
+    //     /shell.exec('sh ./gitauto.sh');
+    // });
+    res.render('index.ejs');
+});
+
+app.post('/submit', (req, res) => {
+    console.log(req.body.textfield);
+    res.redirect('/');
 });
 
 app.listen(8000, () => {
