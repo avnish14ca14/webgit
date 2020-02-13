420aafc456075a2fd0612456035fa96bd5fc21e9
avnishkaushik111 <avnishkaushik111@gmail.com>
2020-02-14 01:01:52 +0530

    git auto add, commit and push

--- index.js
+++ index.js
@@ -11,17 +11,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended : false }));
 
 app.get('/', (req, res) => {
-    // fs.chmod('./gitauto.sh', 0o666, err => {
-    //     if(err) throw err;
-    //     console.log('File permission changed');
-    //     /shell.exec('sh ./gitauto.sh');
-    // });
     res.render('index.ejs');
 });
 
-app.post('/submit', (req, res) => {
-    console.log(req.body.textfield);
+app.post('/submit', (req, res, next) => {
+    if(req.body.textfield.length){
+        fs.chmod('./gitauto.sh', 0o666, err => {
+            if(err) throw err;
+            console.log('File permission changed');
+            shell.exec('sh ./gitauto.sh');
+        });
+        res.redirect('/viewpage/?name='+req.body.textfield);
+        return;
+    }
     res.redirect('/');
+});
+
+app.get('/viewpage', (req, res) => {
+    res.render('viewpage.ejs', { name : req.query.name});
 });
 
 app.listen(8000, () => {
