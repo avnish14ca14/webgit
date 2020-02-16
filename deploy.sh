a76fc5ee695134402708d7e1e0d46fae93133f68
avnishkaushik111 <avnishkaushik111@gmail.com>
2020-02-17 03:23:50 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ssh -tt cloud_user@avnishkumarkaushik3c.mylabserver.com <<EOF
-  hostname
-  cd /devops/webgit/
-  git pull origin master
-  npm install - y
-  npm start
-  exit
+hostname
+cd /devops/webgit/
+git pull
+npm install - production
+pm2 restart all
+exit
 EOF
