e55f5f00a59afee52effbb45d29c1394968be89b
avnishkaushik111 <avnishkaushik111@gmail.com>
2020-02-17 03:34:49 +0530

    final commit

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ssh -tt cloud_user@avnishkumarkaushik3c.mylabserver.com <<EOF
 hostname
 cd /devops/webgit/
 git pull
-npm install - production
-pm2 restart all
+npm install -y
+BUILD_ID=dontKillMe npm start &
 exit
 EOF
